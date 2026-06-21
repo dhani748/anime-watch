40d2d8f5bde64a6a5ca2266c76557af4c52845c9
Dhananjay <qamis2004@gmail.com>
2026-06-21 17:50:42 +0530

    yes

--- src/main/java/com/animeSite/config/DatabaseConfig.java
+++ src/main/java/com/animeSite/config/DatabaseConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 import java.net.URI;
@@ -23,8 +24,8 @@ public class DatabaseConfig {
 
     @Bean
     @Primary
-    public DataSource dataSource(DataSourceProperties properties) {
-        String databaseUrl = System.getenv("DATABASE_URL");
+    public DataSource dataSource(DataSourceProperties properties, Environment environment) {
+        String databaseUrl = environment.getProperty("DATABASE_URL");
         if (databaseUrl != null && !databaseUrl.isEmpty()) {
             try {
                 URI uri = new URI(databaseUrl);
@@ -36,7 +37,22 @@ public class DatabaseConfig {
                         properties.setPassword(creds[1]);
                     }
                 }
-                properties.setUrl("jdbc:postgresql://" + uri.getHost() + ":" + uri.getPort() + uri.getPath());
+                StringBuilder jdbcUrl = new StringBuilder("jdbc:postgresql://");
+                jdbcUrl.append(uri.getHost());
+                if (uri.getPort() > 0) {
+                    jdbcUrl.append(":").append(uri.getPort());
+                }
+                jdbcUrl.append(uri.getPath());
+                String query = uri.getQuery();
+                if (query != null) {
+                    jdbcUrl.append("?").append(query);
+                    if (!query.contains("sslmode")) {
+                        jdbcUrl.append("&sslmode=require");
+                    }
+                } else {
+                    jdbcUrl.append("?sslmode=require");
+                }
+                properties.setUrl(jdbcUrl.toString());
             } catch (URISyntaxException e) {
                 // Fallback to properties from application.yml
             }
